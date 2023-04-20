9c478be3b621a76f5e734810786f538fa49224aa
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2023-04-20 11:35:50 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {ChartSelectionComponent} from "./chart-selection/chart-selection.component";
+import {InputFile} from "./classes/input-file";
+import {fromCSV} from "data-forge";
+import {DataService} from "./services/data.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'curtainFlex';
+export class AppComponent implements OnInit{
+  title = 'Curtain Flex';
+  settings: any = {files: {}}
+  data: {files: Map<string, InputFile>, filenameList: string[]} = {files: new Map<string, InputFile>(), filenameList: []}
+
+
+  constructor(private modal: NgbModal, private dataService: DataService) { }
+
+  ngOnInit(): void {
+
+  }
+  handleFile(e: Event) {
+    if (e.target) {
+      const target = e.target as HTMLInputElement;
+      if (target.files) {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          const loadedFile = reader.result;
+          if (target.files) {
+            this.data.files.set(target.files[0].name, new InputFile(fromCSV(<string>loadedFile), target.files[0].name, <string>loadedFile))
+            this.data.filenameList.push(target.files[0].name)
+          }
+        }
+        reader.readAsText(target.files[0]);
+      }
+    }
+  }
+
+  openChartSelection() {
+    const modalRef = this.modal.open(ChartSelectionComponent);
+    modalRef.componentInstance.data = this.data
+    modalRef.closed.subscribe((result) => {
+      const results = this.dataService.processForm(result.data, result.form, result.plotType)
+      const defaultSettings = this.dataService.getDefaultsPlotOptions(result.plotType)
+      const plotSettings:any = {df: results.data, form: result.form, settings: defaultSettings, plotType: result.plotType}
+      this.dataService.addPlotToList(plotSettings)
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,49 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgbActiveModal, NgbDropdown, NgbDropdownModule, NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ChartSelectionComponent } from './chart-selection/chart-selection.component';
+import * as PlotlyJS from 'plotly.js-dist-min';
+import { PlotlyModule } from 'angular-plotly.js';
+import { DataManagementComponent } from './data-management/data-management.component';
+import { VolcanoPlotComponent } from './plots/volcano-plot/volcano-plot.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {VolcanoPlotFormComponent} from "./chart-selection/volcano-plot-form/volcano-plot-form.component";
+import { HeatmapFormComponent } from './chart-selection/heatmap-form/heatmap-form.component';
+import { ScatterPlotFormComponent } from './chart-selection/scatter-plot-form/scatter-plot-form.component';
+import { BarChartFormComponent } from './chart-selection/bar-chart-form/bar-chart-form.component';
+import { LineChartFormComponent } from './chart-selection/line-chart-form/line-chart-form.component';
+import { BoxPlotFormComponent } from './chart-selection/box-plot-form/box-plot-form.component';
+import { PlotContainerComponent } from './plots/plot-container/plot-container.component';
+import { PlotCompositorComponent } from './plots/plot-compositor/plot-compositor.component';
 
+PlotlyModule.plotlyjs = PlotlyJS;
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartSelectionComponent,
+    DataManagementComponent,
+    VolcanoPlotComponent,
+    VolcanoPlotFormComponent,
+    HeatmapFormComponent,
+    ScatterPlotFormComponent,
+    BarChartFormComponent,
+    LineChartFormComponent,
+    BoxPlotFormComponent,
+    PlotContainerComponent,
+    PlotCompositorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    PlotlyModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    NgbActiveModal
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
